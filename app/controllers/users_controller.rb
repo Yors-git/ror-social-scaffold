6f1aa524a5d402b3a258f82b1701a2a0f5e6eafb
Yors <jatbribiesca@gmail.com>
2020-12-17 16:41:05 -0600

    show accept friend button only for the correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show create_freindship accept]
   def index
     @users = User.all
-    
   end
-  
+
   def show
     @posts = @user.posts.ordered_by_most_recent
     @friends = @user.friends
@@ -13,27 +12,25 @@ class UsersController < ApplicationController
     @pending_inv = @user.friend_requests_to_me
   end
 
-
   def create_freindship
     current_user.friendships.create(user_id: current_user.id, friend_id: @user.id, confirmed: false)
     redirect_to request.referrer, notice: 'Friend request sent'
   end
-  
+
   def accept
     @posts = @user.posts.ordered_by_most_recent
     @pending_inv = @user.friend_requests_to_me
     data = params[:data]
-    
+
     user = User.find(data)
-    
+
     @user.confirm_friend(user)
     redirect_to request.referrer, notice: 'Friend request confirmed'
   end
-  
+
   private
-  
+
   def set_user
     @user = User.find(params[:id])
   end
 end
-
