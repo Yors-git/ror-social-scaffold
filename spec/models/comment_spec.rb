9ce42ee1819869a93bc73dc92d0fda7b75aeb280
Yors <jatbribiesca@gmail.com>
2020-12-19 20:37:02 -0600

    add rspec for comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Comment', type: :model do
+  let(:user1) { User.create(name: 'RSpec', email: 'rspec@test.com', password: '123456') }
+  let(:post1) { Post.create(user_id: user1.id, content: 'test post content por Rspec') }
+  let(:comment) { Comment.create(user_id: user1.id, post_id: post1.id, content: 'test comment content por Rspec') }
+  let(:inv_comment) { Comment.create(user_id: user1.id, post_id: post1.id, content: '') }
+  let(:user) { Comment.reflect_on_association(:user).macro }
+  let(:post) { Comment.reflect_on_association(:post).macro }
+
+  it 'checks if creating comment is correct' do
+    expect(comment).to be_valid
+  end
+
+  it 'check user name validation' do
+    expect(inv_comment).to_not be_valid
+  end
+
+  it 'check correct association between user and comments' do
+    expect(user).to eq(:belongs_to)
+  end
+
+  it 'check correct association between post and comments' do
+    expect(post).to eq(:belongs_to)
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'Post', type: :model do
   let(:post) { Post.create(user_id: user1.id, content: 'test post content por Rspec') }
   let(:inv_post) { Post.create(user_id: user1.id, content: '') }
   let(:user) { Post.reflect_on_association(:user).macro }
-  let(:comments) { User.reflect_on_association(:comments).macro }
-  let(:likes) { User.reflect_on_association(:likes).macro }
+  let(:comments) { Post.reflect_on_association(:comments).macro }
+  let(:likes) { Post.reflect_on_association(:likes).macro }
 
   it 'checks if creating post is correct' do
     expect(post).to be_valid
@@ -16,15 +16,15 @@ RSpec.describe 'Post', type: :model do
     expect(inv_post).to_not be_valid
   end
 
-  it 'check correct association between posts and user' do
+  it 'check correct association between user and post' do
     expect(user).to eq(:belongs_to)
   end
 
-  it 'check correct association between comments and user' do
+  it 'check correct association between comments and post' do
     expect(comments).to eq(:has_many)
   end
 
-  it 'check correct association between likes and user' do
+  it 'check correct association between likes and post' do
     expect(likes).to eq(:has_many)
   end
 end
