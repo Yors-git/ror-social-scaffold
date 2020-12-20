58ca9147fd721c21d33ba16598f1a8a21e267383
Yors <jatbribiesca@gmail.com>
2020-12-20 14:15:36 -0600

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def check_current_user
-    sign_out_btn =  (link_to 'Sign out', destroy_user_session_path, method: :delete)
+    sign_out_btn = (link_to 'Sign out', destroy_user_session_path, method: :delete)
     if signed_in?
       (link_to "Logged as: #{current_user.name}", user_path(current_user), method: :get) + sign_out_btn
     else
@@ -25,13 +25,14 @@ module ApplicationHelper
     end
   end
 
-  def show_messages    
-    result = ''    
-    result += "<div class='notice'><p> #{notice} </p></div>"  if notice.present?
+  def show_messages
+    result = ''
+    result += "<div class='notice'><p> #{notice} </p></div>" if notice.present?
     result += "<div class='notice'><p> #{alert} </p></div>" if alert.present?
-    result.html_safe  
+    result.html_safe
   end
 
+  # rubocop: disable Style/GuardClause, Layout/LineLength
   def invite_friend_index(user)
     if !user.friends.include?(current_user) && user != current_user && !user.pending_friends.include?(current_user) && !user.friend_requests.include?(current_user)
       button_to 'Invite friend', create_freindship_user_path(user.id), method: :get
@@ -39,15 +40,16 @@ module ApplicationHelper
   end
 
   def invite_friend_show(user, pending_inv, pending_req)
-    if !user.friends.include?(current_user) && user.id != current_user.id  && !pending_inv.include?(current_user) && !pending_req.include?(current_user)
-      button_to 'Invite friend', create_freindship_user_path, :method => :get
+    if !user.friends.include?(current_user) && user.id != current_user.id && !pending_inv.include?(current_user) && !pending_req.include?(current_user)
+      button_to 'Invite friend', create_freindship_user_path, method: :get
     end
   end
 
   def accept_and_decline_btns(usr)
-    if @user.id == current_user.id 
+    if @user.id == current_user.id
       (button_to 'Accept', accept_user_path, method: :get, params: { data: usr }) +
-      (button_to 'Decline', decline_user_path, method: :get, params: { data: usr })
+        (button_to 'Decline', decline_user_path, method: :get, params: { data: usr })
     end
   end
+  # rubocop: enable Style/GuardClause, Layout/LineLength
 end
